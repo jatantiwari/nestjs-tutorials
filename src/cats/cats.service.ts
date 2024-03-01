4099e1269f15a015123dcd4ef18a6dd2961afdc5
jatantiwari <jatantiwarijt@gmail.com>
2024-03-01 18:52:48 +0530

    excesption filter

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Cats } from "./interface/cats.interface";
 
 @Injectable()
 export class CatsServices{
   private readonly cats: Cats[] = [];
   create(cat:Cats){
+    console.log(cat.name!=undefined)
+    if(cat.name==undefined || cat.breed==undefined ||cat.age==undefined || cat.id==undefined){
+      throw new HttpException('Not Acceptable',HttpStatus.NOT_ACCEPTABLE)
+    }
     this.cats.push(cat);
+
+    // throw new HttpException('Forbidden',HttpStatus.FORBIDDEN)
+    // we can also send custom message
+    // throw new HttpException({
+    //   status: HttpStatus.FORBIDDEN,
+    //   message: 'This is a custom message',
+    // }, HttpStatus.FORBIDDEN, {
+    //   cause: cat
+    // });
+
+    throw new BadRequestException('Something bad happened', { cause: new Error(), description: 'Some error description' })
   }
 
   findAll():Cats[]{
